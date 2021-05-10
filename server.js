0b6a3778e3eadc699fd8b630b5462ed8def10094
Fikri Ihsan <fikri@192.168.1.32>
2021-05-10 23:18:17 +0700

    create new routes

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+
+const blogForm = require("./routes/blogForm");
+
+app.use(blogForm);
 
 app.listen(3001, () => {
   console.log("server.js running on port 3001");
