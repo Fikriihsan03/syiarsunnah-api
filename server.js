ccfa6a7440ae9cad923b70d3ad1e9a636cde5d0a
Fikri ihsan <mfikriihsan03@gmail.com>
2021-07-10 16:31:17 +0700

    add login panel;

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+// const path = require("./util/path")
+const path = require("path")
 const app = express();
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 const blogForm = require("./routes/blogForm");
 const blogData = require("./routes/blogData");
