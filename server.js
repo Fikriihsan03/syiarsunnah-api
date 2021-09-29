d40032506252730089c45938a7567413021d85b4
Fikri ihsan <mfikriihsan03@gmail.com>
2021-09-29 21:14:11 +0700

    fix variable name

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const db = require("./db");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
 var MySQLStore = require("express-mysql-session")(session);
-const fileupload = require("express-fileupload")
+const fileUpload = require("express-fileupload")
 
 const app = express();
-app.use(fileupload())
+app.use(fileUpload())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
