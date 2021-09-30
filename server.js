bb6d600c57fc73d414bebedd85037d886972060d
Fikri ihsan <mfikriihsan03@gmail.com>
2021-09-30 13:07:25 +0700

    fix error when upload file too large

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ var MySQLStore = require("express-mysql-session")(session);
 
 const app = express();
 // app.use(fileUpload());
-app.use(express.json());
+app.use(express.json({limit: '25mb'}));
+app.use(express.urlencoded({limit: '25mb', extended: true}));
+// app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 var options = {
